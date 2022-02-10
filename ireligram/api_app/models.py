527cd12cb56e92bcb59933175d58a85c48c37736
fred <fredblade142@gmail.com>
2022-02-10 09:45:32 -0500

    image

--- ireligram/api_app/models.py
+++ ireligram/api_app/models.py
@@ -6,5 +6,5 @@ class PostItem(models.Model):
     title = models.CharField(max_length=100)
     body = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
-    # image = models.CharField(max_length=100, null=True)
+    image = models.CharField(max_length=100, null=True)
     date_created = models.DateField(auto_now=True)
